e7a592336cb2ca471d938b4f7aadeafd3875210d
SimpleCode <xq@123456789deimac.local>
2018-03-06 15:36:04 +0800

    Initial Commit

---
+++ SPMarqueeViewExample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SPMarqueeViewExample
+//
+//  Created by 123456789 on 2018/3/6.
+//  Copyright © 2018年 123456789. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
